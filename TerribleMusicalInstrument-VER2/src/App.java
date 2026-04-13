9f00db2d32e60b7a27e8fd86b8ab90e16450914a
Anthony Coughlan <anthonycoughlan07@gmail.com>
2026-04-13 22:45:26 +0100

    few more changes

--- TerribleMusicalInstrument-VER2/src/App.java
+++ TerribleMusicalInstrument-VER2/src/App.java
@@ -1,14 +1,50 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
         SoundEngine soundEngine = new SoundEngine();
         KeyHandler keyHandler = new KeyHandler(soundEngine);//implements the soundEngine to keyHandler constructor
-        JFrame frame = new JFrame();
-        frame.setSize(500,500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Border border = BorderFactory.createLineBorder(Color.green,3);
+
+        Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("TerribleMusicalInstrument-VER2/WhimsyTT.ttf")).deriveFont(35f);
+        JFrame frame = new JFrame();//creates a frame
+        JPanel titlePanel = new JPanel();
+        frame.setSize(1200,900);//sets dimenstion
+
+        titlePanel.setBounds(400, 0, 400, 300);
+
+        titlePanel.setLayout(null);
+
+        JLabel label = new JLabel();//creates a label
+        label.setText("The Amazing Piano!");
+        label.setBounds(0, 0, 400, 60);
+        //label.setBorder(border);
+        label.setVerticalAlignment(JLabel.TOP);//set text/image to top of label
+        label.setHorizontalAlignment(JLabel.CENTER);//set text/image to center of label
+        //label.setBounds(JLabel.CENTER,JLabel.TOP,250,100);
+        //label.setHorizontalTextPosition(JLabel.CENTER);
+        //label.setVerticalTextPosition(JLabel.TOP);
+        //label.setForeground(new Color(0,0,0));
+        label.setFont(customFont); //set font of text
+        //label.setIconTextGap(50); //set gap of text to image
+        //label.setBackground(Color.black);
+        //label.setOpaque(true);
+        titlePanel.add(label);
+
+
+        frame.setTitle("The Amazing Piano!");
+        frame.setLayout(null);
+        //frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);//tricks the user into closing the application
         frame.addKeyListener(keyHandler);//connects the keyHandler class directly to the frame
         frame.setFocusable(true);
-        frame.setVisible(true);
+        frame.add(titlePanel);
+        frame.setVisible(true);//ensures we can actually see the frame
+        //frame.pack();
     }
 }
--- TerribleMusicalInstrument-VER2/src/KeyHandler.java
+++ TerribleMusicalInstrument-VER2/src/KeyHandler.java
@@ -32,37 +32,37 @@ public class KeyHandler implements KeyListener{ //KeyListener acts like a contra
             dHeld = true; //this will stop the method repeating when the Key is pressed down, now acts like a real piano
             pitchD = (int)(Math.random() * 128);
             int velocity = (int)(Math.random() * 101);
-            soundEngine.playNote(1, pitchD, velocity);
+            soundEngine.playNote(3, pitchD, velocity);
         }
         if(key == KeyEvent.VK_F && !fHeld){
             fHeld = true; //this will stop the method repeating when the Key is pressed down, now acts like a real piano
             pitchF = (int)(Math.random() * 128);
             int velocity = (int)(Math.random() * 101);
-            soundEngine.playNote(1, pitchF, velocity);
+            soundEngine.playNote(4, pitchF, velocity);
         }
         if(key == KeyEvent.VK_J && !jHeld){
             jHeld = true; //this will stop the method repeating when the Key is pressed down, now acts like a real piano
             pitchJ = (int)(Math.random() * 128);
             int velocity = (int)(Math.random() * 101);
-            soundEngine.playNote(1, pitchJ, velocity);
+            soundEngine.playNote(5, pitchJ, velocity);
         }
         if(key == KeyEvent.VK_K && !kHeld){
             kHeld = true; //this will stop the method repeating when the Key is pressed down, now acts like a real piano
             pitchK = (int)(Math.random() * 128);
             int velocity = (int)(Math.random() * 101);
-            soundEngine.playNote(1, pitchK, velocity);
+            soundEngine.playNote(6, pitchK, velocity);
         }
         if(key == KeyEvent.VK_L && !lHeld){
             lHeld = true; //this will stop the method repeating when the Key is pressed down, now acts like a real piano
             pitchL = (int)(Math.random() * 128);
             int velocity = (int)(Math.random() * 101);
-            soundEngine.playNote(1, pitchL, velocity);
+            soundEngine.playNote(7, pitchL, velocity);
         }
         if(key == KeyEvent.VK_SEMICOLON && !semiHeld){
             semiHeld = true; //this will stop the method repeating when the Key is pressed down, now acts like a real piano
             pitchSemi = (int)(Math.random() * 128);
             int velocity = (int)(Math.random() * 101);
-            soundEngine.playNote(1, pitchSemi, velocity);
+            soundEngine.playNote(8, pitchSemi, velocity);
         }
     }
 
@@ -80,27 +80,27 @@ public class KeyHandler implements KeyListener{ //KeyListener acts like a contra
         }
         if(key == KeyEvent.VK_D){
             dHeld = false; //once key is lifted resets the variable so the logic true again for KeyPressed
-            soundEngine.stopNote(1, pitchD);
+            soundEngine.stopNote(3, pitchD);
         }
         if(key == KeyEvent.VK_F){
             fHeld = false; //once key is lifted resets the variable so the logic true again for KeyPressed
-            soundEngine.stopNote(1, pitchF);
+            soundEngine.stopNote(4, pitchF);
         }
         if(key == KeyEvent.VK_J){
             jHeld = false; //once key is lifted resets the variable so the logic true again for KeyPressed
-            soundEngine.stopNote(1, pitchJ);
+            soundEngine.stopNote(5, pitchJ);
         }
         if(key == KeyEvent.VK_K){
             kHeld = false; //once key is lifted resets the variable so the logic true again for KeyPressed
-            soundEngine.stopNote(1, pitchK);
+            soundEngine.stopNote(6, pitchK);
         }
         if(key == KeyEvent.VK_L){
             lHeld = false; //once key is lifted resets the variable so the logic true again for KeyPressed
-            soundEngine.stopNote(1, pitchL);
+            soundEngine.stopNote(7, pitchL);
         }
         if(key == KeyEvent.VK_SEMICOLON){
             semiHeld = false; //once key is lifted resets the variable so the logic true again for KeyPressed
-            soundEngine.stopNote(1, pitchSemi);
+            soundEngine.stopNote(8, pitchSemi);
         }
     }
     @Override//must define this method as this interface acts as a contract promising all three methods will be present
